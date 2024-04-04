6fa8108f0b56164568b0d9e4303088c5122df25f
epxzzy <parv66@outlook.com>
2024-04-04 14:37:12 +0530

    Add files via upload

---
+++ err.js
@@ -0,0 +1,8 @@
+
+function err() { 
+      var element = document.getElementById("errl");
+      element.classList.add("errl");
+            var audio = document.getElementById("errors");
+            audio.play();
+            audio.volume = 0.2;
+}
